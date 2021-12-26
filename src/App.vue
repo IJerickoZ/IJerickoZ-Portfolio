33caa59ec6a3baa03c921d74bc46ae563cad9634
IJerickoZ <piya.pkk@outlook.com>
2021-12-27 06:43:02 +0700

    create project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Introduce />
+  </div>
+</template>
+
+<script>
+import Introduce from './sections/Introduce.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Introduce
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/sections/Introduce.vue
@@ -0,0 +1,48 @@
+<template>
+    <section class="hero is-halfheight" style="background-color: #1f2833">
+        <div class="columns mt-5">
+                <div class="column is-4 mt-5">
+                    <div class="image">
+                        <img class="is-rounded " src="../assets/pic1.jpg" style="width: 256px;float: right">
+                    </div>
+                </div>
+                <div class="hero-body">
+                    <div class="column is-4">
+                        <p class="title" style="color:white">
+                            Piya Kaewket
+                        </p>
+                        <hr>
+                        <p class="subtitle mt-3" style="color:white">
+                            Hello, I'm Toey. A full-stack web developer. I am now mainly working 
+                            with JavaScript and related technologies.
+                            I am also comfortable with NodeJS I love trying new things, 
+                            sharing ideas, inspiring people, getting inspiredand meeting new people.
+                            <br>
+                        </p>
+
+                        <span class="icon">
+                            <i class="fas fa-envelope" style="color:white;font-size:20px"></i>
+                        </span>
+                        <span><strong style="color:white;font-size:16px">: Piya.pkk@outlook.com</strong></span><br>
+
+                        <a href="https://github.com/IJerickoZ" target="_blank">
+                            <span class="icon mt-2">
+                                <i class="fab fa-github" style="color:white;font-size:20px"></i>
+                            </span>
+                            <span><strong style="color:white;font-size:16px">: IJerickoZ</strong></span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+    </section>
+    
+</template>
+
+<script>
+export default {
+    name: "Test",
+}
+</script>
+
+<style>
+</style>
